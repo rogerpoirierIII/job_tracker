116475a786b3ea599115597fd1a88501608cc61c
Roger Poirier <rpoiri2@wgu.edu>
2025-08-20 21:46:38 -0500

    Initialized Flask app and added user and job models

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view =  'main.login' #Redirects page to login page is authentications fails
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    from .routes import main
+    app.register_blueprint(main)
---
+++ app/models.py
@@ -0,0 +1,28 @@
+from enum import unique
+
+from sqlalchemy import false
+from sqlalchemy.orm import backref
+
+from . import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(UserMixin,db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150),unique=True, nullable=False)
+    email = db.Column(db.String(150),unique=True, nullable=False)
+    password_hash = db.Column(db.String(256),nullable=False)
+    jobs = db.relationship('Job',backref='owner',lazy=True)
+
+    def set_password(self,password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,password):
+        return check_password_hash(password)
+
+class Job(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    company = db.Column(db.String(150), nullable=False)
+    status = db.Column(db.String(50), default='Applied')
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
