318d83e3100c7015ebcfc09d947256d18c2d0a2c
Roger Poirier <rpoiri2@wgu.edu>
2025-09-08 21:52:23 -0500

    adds initial migration and fixes migrations setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 db = SQLAlchemy()
+migrate = Migrate()
 login_manager = LoginManager()
 login_manager.login_view =  'main.login' #Redirects page to login page is authentications fails
 
@@ -14,6 +16,7 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+    migrate.init_app(app)
     login_manager.init_app(app)
 
     from .routes import main
