52f5823fb77a8d1d7fdede084b18aaf4054a4b5c
Roger Poirier <rpoiri2@wgu.edu>
2025-09-08 22:00:03 -0500

    adds migrations folder

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
-from app import create_app, db
-from app.models import User, Job
+from app import create_app
 
 app = create_app()
-
-# # Create database tables if they don't exist
-# with app.app_context():
-#     db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=False)
