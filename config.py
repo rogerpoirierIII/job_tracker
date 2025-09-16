55cf03d2ba35f52424577d289f31a8ea186771f1
Roger Poirier <rpoiri2@wgu.edu>
2025-09-16 10:49:35 -0500

    database URL fix

--- config.py
+++ config.py
@@ -2,5 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    uri = os.environ.get("DATABASE_URL")  # From Render
+    if uri and uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
